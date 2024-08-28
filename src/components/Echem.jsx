6f35b479533573a1e2b5f09e13984ffd5142f331
Vinny Kylex <vinnykylex@gmail.com>
2024-08-28 22:21:00 +0300

    update Aug 28

--- src/components/Echem.jsx
+++ src/components/Echem.jsx
@@ -162,14 +162,10 @@ function Echem({ cartItems, setCartItems }) {
 
 
 
-            <div className="small-footer">
+            {/* <div className="small-footer">
                 <p>&copy; 2024 Echem. All rights reserved.</p>
-                <div className="footer-links">
-                    <a href="/about">About Us</a>
-                    <a href="/contact">Contact</a>
-                    <a href="/privacy">Privacy Policy</a>
-                </div>
-            </div>
+               
+            </div> */}
 
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" transform="rotate(0)">
                 <path fill="#1a1a1a" fillOpacity="1" d="M0,32L80,74.7C160,117,320,203,480,229.3C640,256,800,224,960,229.3C1120,235,1280,277,1360,298.7L1440,320L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
